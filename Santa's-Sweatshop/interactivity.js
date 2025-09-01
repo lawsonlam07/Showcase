fb4b78e87305d1aaef9fd77448a4930d75aa8879
Lawson Lam <109973600+lawsonlam07@users.noreply.github.com>
2025-09-01 22:06:23 +0100

    added mobile scroll snapping for ss

--- Santa's-Sweatshop/interactivity.js
+++ Santa's-Sweatshop/interactivity.js
@@ -1,4 +1,4 @@
-// const container = document.querySelector('.container');
+// const container = document.querySelector('.screenshots');
 // const itemWidth = container.offsetWidth;
 
 // container.addEventListener('scroll', () => {
