22dc0b8efc093ab98cb7d30e5d201554f680040f
Lawson Lam <109973600+lawsonlam07@users.noreply.github.com>
2025-09-03 19:00:27 +0100

    added mobile functionality

--- Santa's-Sweatshop/interactivity.js
+++ Santa's-Sweatshop/interactivity.js
@@ -3,65 +3,65 @@ let imageView = false;
 let imageNum = 0;
 
 function fullscreen() {
-  if (page.requestFullscreen) {
-    page.requestFullscreen();
-  } else if (page.mozRequestFullscreen) {
-    page.mozRequestFullscreen();
-  } else if (page.webkitRequestFullscreen) {
-    page.webkitRequestFullscreen();
-  } else if (page.msRequestFullscreen) {
-    page.msRequestFullscreen();
-  }
+    if (page.requestFullscreen) {
+        page.requestFullscreen();
+    } else if (page.mozRequestFullscreen) {
+        page.mozRequestFullscreen();
+    } else if (page.webkitRequestFullscreen) {
+        page.webkitRequestFullscreen();
+    } else if (page.msRequestFullscreen) {
+        page.msRequestFullscreen();
+    }
 }
 
 function setupImage(e) {
-  imageView = true;
-  imageNum = e.target.dataset.num;
-  displayImage();
+    imageView = true;
+    imageNum = e.target.dataset.num;
+    displayImage();
 }
 
 function prevImage() {
-  imageNum--;
-  imageNum = imageNum >= 0 ? imageNum : 4;
-  displayImage();
+    imageNum--;
+    imageNum = imageNum >= 0 ? imageNum : 4;
+    displayImage();
 }
 
 function nextImage() {
-  imageNum++;
-  imageNum = imageNum <= 4 ? imageNum : 0;
-  displayImage();
+    imageNum++;
+    imageNum = imageNum <= 4 ? imageNum : 0;
+    displayImage();
 }
 
 function displayImage() {
-  let imageScreen = document.getElementById("image-display");
-  let image = document.getElementById("displayed-image");
-  imageScreen.style.display = "flex";
-  image.src = `Game/Promo/screenie${imageNum}.webp`;
+    document.getElementById("image-display").style.display = "block";
+    document.getElementById("displayed-image").src = `Game/Promo/screenie${imageNum}.webp`;
 }
 
-function disableImage() {
-  imageView = false;
-  document.getElementById("image-display").style.display = "none";
+function disableImage(e) {
+    if (e.target.id === "image-display") {
+        imageView = false;
+        document.getElementById("image-display").style.display = "none";
+    }
 }
 
 document.addEventListener("keydown", function (e) {
-  if (imageView) {
-    switch (e.key) {
-      case "ArrowLeft":
-        prevImage();
-        break;
-      case "ArrowRight":
-        nextImage();
-        break;
-      case "Escape":
-        disableImage();
-        break;
+    if (imageView) {
+        switch (e.key) {
+            case "ArrowLeft":
+                prevImage();
+                break;
+            case "ArrowRight":
+                nextImage();
+                break;
+            case "Escape":
+                disableImage();
+                break;
+        }
     }
-  }
 });
 
 document.addEventListener("fullscreenchange", () => {
-  let isFullscreen = document.fullscreenElement !== null;
-  page.style.overflow = isFullscreen ? "hidden" : "visible";
-  document.querySelector("#game").style.display = isFullscreen ? "block" : "none";
+    let isFullscreen = document.fullscreenElement !== null;
+    page.style.overflow = isFullscreen ? "hidden" : "visible";
+    document.querySelector("#game").style.display = isFullscreen ? "block" : "none";
 });
