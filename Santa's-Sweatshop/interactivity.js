a2fd7effd3d0f9d047f0fe4a7002cc3efbd7ae9c
Lawson Lam <109973600+lawsonlam07@users.noreply.github.com>
2025-09-03 15:53:59 +0100

    added keyboard functionality to switch between display of screenshots

--- Santa's-Sweatshop/interactivity.js
+++ Santa's-Sweatshop/interactivity.js
@@ -1,31 +1,67 @@
-const page = document.documentElement
+const page = document.documentElement;
+let imageView = false;
+let imageNum = 0;
 
 function fullscreen() {
-    if (page.requestFullscreen) {
-        page.requestFullscreen()
-    } else if (page.mozRequestFullscreen) {
-        page.mozRequestFullscreen()
-    } else if (page.webkitRequestFullscreen) {
-        page.webkitRequestFullscreen()
-    } else if (page.msRequestFullscreen) {
-        page.msRequestFullscreen()
-    }
+  if (page.requestFullscreen) {
+    page.requestFullscreen();
+  } else if (page.mozRequestFullscreen) {
+    page.mozRequestFullscreen();
+  } else if (page.webkitRequestFullscreen) {
+    page.webkitRequestFullscreen();
+  } else if (page.msRequestFullscreen) {
+    page.msRequestFullscreen();
+  }
 }
 
-function displayImage(e) {
-    let imageScreen = document.getElementById("image-display")
-    let image = document.getElementById("displayed-image")
-    console.log(e.target.src)
-    imageScreen.style.display = "block"
-    image.src = e.target.src
+function setupImage(e) {
+  imageView = true;
+  imageNum = e.target.dataset.num;
+  displayImage();
+}
+
+function prevImage() {
+  imageNum--;
+  imageNum = imageNum >= 0 ? imageNum : 4;
+  displayImage();
+}
+
+function nextImage() {
+  imageNum++;
+  imageNum = imageNum <= 4 ? imageNum : 0;
+  displayImage();
+}
+
+function displayImage() {
+  let imageScreen = document.getElementById("image-display");
+  let image = document.getElementById("displayed-image");
+  imageScreen.style.display = "flex";
+  image.src = `Game/Promo/screenie${imageNum}.webp`;
 }
 
 function disableImage() {
-    document.getElementById("image-display").style.display = "none"
+  imageView = false;
+  document.getElementById("image-display").style.display = "none";
 }
 
+document.addEventListener("keydown", function (e) {
+  if (imageView) {
+    switch (e.key) {
+      case "ArrowLeft":
+        prevImage();
+        break;
+      case "ArrowRight":
+        nextImage();
+        break;
+      case "Escape":
+        disableImage();
+        break;
+    }
+  }
+});
+
 document.addEventListener("fullscreenchange", () => {
-  let isFullscreen = document.fullscreenElement !== null
-  page.style.overflow = isFullscreen ? "hidden" : "visible"
-  document.querySelector("#game").style.display = isFullscreen ? "block" : "none"
+  let isFullscreen = document.fullscreenElement !== null;
+  page.style.overflow = isFullscreen ? "hidden" : "visible";
+  document.querySelector("#game").style.display = isFullscreen ? "block" : "none";
 });
